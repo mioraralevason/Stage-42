bbdaa6fd35b2d7160cd214279afd853d8c223bf6
Ralevason Miora <144311101+mioraralevason@users.noreply.github.com>
2025-08-26 16:52:42 +0300

    push integr tsy vita

---
+++ 42/src/main/java/com/ecole/_2/models/Cursus.java
@@ -0,0 +1,23 @@
+package com.ecole._2.models;
+
+public class Cursus {
+    private int id;
+    private String name;
+
+    // Getters et Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
