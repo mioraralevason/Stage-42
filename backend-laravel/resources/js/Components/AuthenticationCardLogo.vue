82befbe847efebcd3d09ee640a6dd2402eb6e0f7
tiavinaaroniaina <169129873+tiavinaaroniaina@users.noreply.github.com>
2026-02-07 14:59:16 +0300

    last

--- backend-laravel/resources/js/Components/AuthenticationCardLogo.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <Link href="/">
-        <img src="/logo.png" alt="Garage Simulation" class="h-12 w-auto" />
-        <!-- ou juste du texte -->
-        <!-- <span class="text-2xl font-bold text-indigo-600">Garage Sim</span> -->
-    </Link>
-</template>
